94982c7aa528ca9f05bc4a4bb0f5234243eb14f5
r888800009 <r888800009@users.noreply.github.com>
2018-02-23 21:14:01 +0800

    Update Hello World

--- HelloWorld/Main.lua
+++ HelloWorld/Main.lua
@@ -1,3 +1,3 @@
 local MineOSCore = require("MineOSCore")
 local Localization = MineOSCore.getCurrentApplicationLocalization() 
-require("GUI").error(Localization.helloworld)
+require("GUI").error(Localization.helloWorld)
